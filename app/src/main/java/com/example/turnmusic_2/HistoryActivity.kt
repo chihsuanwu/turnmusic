154000f118036d364efb2bbebedb0b6c99a8839e
freedomtimer <you@example.com>
2020-11-05 16:46:13 +0800

    我拿掉 finish 改強橫向 跟menu按鈕間距

--- app/src/main/java/com/example/turnmusic_2/HistoryActivity.kt
+++ app/src/main/java/com/example/turnmusic_2/HistoryActivity.kt
@@ -6,14 +6,24 @@ import android.os.Bundle
 import android.view.View
 
 class HistoryActivity : AppCompatActivity() {
+
+    lateinit var resultArray: BooleanArray
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        resultArray = intent.getBooleanArrayExtra("result")!!
+
+        val title = intent.getStringExtra("title")!!
+        val fileName = intent.getStringExtra("fileName")!!
+
+        val correct = resultArray.count { it }
+        val percentage = (correct*1.0/resultArray.size) * 100
     }
 
     fun meun_result_click(view: View) {
         startActivity(Intent(this@HistoryActivity,MenuActivity::class.java))
-        finish()
     }
 
     fun exit_result_click(view: View) {
